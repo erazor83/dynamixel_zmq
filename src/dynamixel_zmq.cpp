a5c777fbc9c4a35a9c50556d237de0c089599a3a
Alexander "erazor" Krause <alexander.krause@ed-solutions.de>
2013-09-21 00:15:12 +0200

    implemented most parts of zeromq interface
    done:
      * DYNAMIXEL_RQ_PING
      * DYNAMIXEL_RQ_WRITE_DATA
      * DYNAMIXEL_RQ_REG_WRITE
      * DYNAMIXEL_RQ_REG_ACTION
      * DYNAMIXEL_RQ_RESET
    
    Typechecking and error codes added.
    Check for invalid lists.

--- src/dynamixel_zmq.cpp
+++ src/dynamixel_zmq.cpp
@@ -18,6 +18,20 @@
 
 
 #include "dynamixel.h"
+typedef enum {
+	/* default dynamixel commands */
+	DYNAMIXEL_RQ_PING					=0x01,
+	DYNAMIXEL_RQ_READ_DATA		=0x02,
+	DYNAMIXEL_RQ_WRITE_DATA		=0x03,
+	DYNAMIXEL_RQ_REG_WRITE		=0x04,
+	DYNAMIXEL_RQ_REG_ACTION		=0x05,
+	DYNAMIXEL_RQ_RESET				=0x06,
+	DYNAMIXEL_RQ_SYNC_WRITE		=0x83,
+	
+	/* custom commands */
+	DYNAMIXEL_RQ_ZMQ_ECHO			=0xF0,
+	
+} dynamixel_request_t;
 
 #define DESCRIPTION "dyn_zmq - Dynamixel ZeroMQ service"
 namespace { 
@@ -26,6 +40,15 @@ namespace {
   const size_t ERROR_UNHANDLED_EXCEPTION = 2; 
  
 }
+
+typedef enum {
+	ZMQ_ERR_NO_ERROR						= 0,
+	ZMQ_ERR_INVALID_COMMAND			= -1000,
+	ZMQ_ERR_INVALID_FORMAT			= -1001,
+	ZMQ_ERR_INVALID_PARAMETERS	= -1002,
+	ZMQ_ERR_BUS_OFFLINE					= -1003,
+	
+} zmq_error_code_t;
 //using namespace std;
 //using namespace dynapi;
 
@@ -35,6 +58,12 @@ int main(int argc, char** argv) {
 	std::string serial_port="/dev/ttyUSB0";
 	std::string interface_type="cm5";
 	uint32_t serial_speed=1000000;
+	
+	bool debug=false;
+	
+	int tx_error_code=0;
+	
+	int dynamixel_ret=0;
 	
 	namespace po = boost::program_options;
 
@@ -47,6 +76,7 @@ int main(int argc, char** argv) {
 		("speed", po::value< uint32_t >( &serial_speed ),			"serial speed      | default: 1000000" )
 		("type", po::value< std::string >( &interface_type ),	"interface type    | default: cm5" )
 		("dynamixel-scan", "scan for dynamixel servos")
+		("debug", "print out debugging info")
 	;
 
   po::variables_map vm;
@@ -59,19 +89,22 @@ int main(int argc, char** argv) {
 			std::cout << DESCRIPTION << std::endl << desc << std::endl; 
 			return SUCCESS; 
 		}
-
+		
+		if (vm.count("debug")) {
+			debug=true;
+		}
 	} catch(po::error& e) {
 		std::cerr << "ERROR: " << e.what() << std::endl << std::endl; 
 		std::cerr << desc << std::endl; 
 		return ERROR_IN_COMMAND_LINE; 
 	} 
 
-#if 1
-	std::cout << "uri   = " << zmq_uri << std::endl; 
-	std::cout << "port  = " << serial_port << std::endl;
-	std::cout << "speed = " << serial_speed << std::endl;
-#endif
-
+	if (debug) {
+		std::cout << "uri   = " << zmq_uri << std::endl; 
+		std::cout << "port  = " << serial_port << std::endl;
+		std::cout << "speed = " << serial_speed << std::endl;
+	}
+	
 	// === dynamixel part ===
 	dynamixel_t *dyn;
 	dyn = dynamixel_new_rtu(serial_port.c_str(), (uint32_t)serial_speed, _DYNAMIXEL_SERIAL_DEFAULTS);
@@ -108,29 +141,168 @@ int main(int argc, char** argv) {
 	zmq::socket_t socket (context, ZMQ_REP);
 	socket.bind (zmq_uri.c_str());
 
-	std::cout << "Server started (uri="<<zmq_uri<<")" << std::endl;
-
+	if (debug) {
+		std::cout << "Server started (uri="<<zmq_uri<<")" << std::endl;
+	}
+	
 	while (true) {
 		zmq::message_t rx_zmq;
-		
+		msgpack::object rx_obj;
+		msgpack::object tx_obj;
+		msgpack::sbuffer tx_msg;
+		msgpack::unpacked rx_msg;
+		std::vector<uint8_t> rx_vect;
+		std::vector<int> tx_vect;
+					
 		//  Wait for next request from client
 		if (socket.recv (&rx_zmq)) {
 			// Deserialize the serialized data.
 
-			msgpack::unpacked rx_msg;
 			msgpack::unpack(&rx_msg, static_cast<char*>(rx_zmq.data()), rx_zmq.size());
-			msgpack::object rx_obj = rx_msg.get();
+			rx_obj = rx_msg.get();
 
 			// Print the deserialized object to stdout.
-			std::cout << rx_obj << std::endl;    // ["Hello," "World!"]
-
+			if (debug) {
+				std::cout << "== rx ==" << std::endl;
+				std::cout << "data: " << rx_obj << std::endl;
+			}
+			//std::cout << rx_obj.get() <<std::endl;
 			
-			msgpack::sbuffer tx_msg;
-			msgpack::pack(&tx_msg, rx_obj);
+			//uint8_t command = rx_obj.get();
+			
+			tx_error_code=ZMQ_ERR_INVALID_FORMAT;
+			if (rx_obj.type==msgpack::type::ARRAY) {
+				try {
+					rx_obj.convert(&rx_vect);
+					tx_error_code=0;
+				} catch(std::bad_cast& e) {
+					std::cerr << "MESSAGE-ERROR: " << e.what() << std::endl << std::endl; 
+				}
+			}
+			
+			if (tx_error_code==0) {
+				if (debug) {
+					std::cout << "command: " << (int)rx_vect.at(0) << std::endl;
+				}
+				
+
+				switch (rx_vect.at(0)) {
+					case DYNAMIXEL_RQ_PING:
+						if (rx_vect.size()!=2) {
+							tx_error_code=ZMQ_ERR_INVALID_PARAMETERS;
+						} else if (dyn_connected==0) {
+							dynamixel_ret=dynamixel_ping(dyn,(uint8_t)rx_vect.at(1));
+							tx_vect.push_back(ZMQ_ERR_NO_ERROR);
+							tx_vect.push_back(dynamixel_ret);
+						} else {
+							tx_error_code=ZMQ_ERR_BUS_OFFLINE;
+						}
+						break;
+
+					case DYNAMIXEL_RQ_READ_DATA:
+						//std::copy(buf,buf+length,std::back_inserter(vec));
+					case DYNAMIXEL_RQ_WRITE_DATA:
+						if (rx_vect.size()>3) {
+							tx_error_code=ZMQ_ERR_INVALID_PARAMETERS;
+						} else if (dyn_connected==0) {
+							dynamixel_ret=dynamixel_write_data(
+								dyn,
+								(uint8_t)rx_vect.at(1),
+								(dynamixel_register_t)rx_vect.at(2),
+								rx_vect.size()-3,
+								static_cast<uint8_t*>(rx_vect.data())
+							);
+							tx_vect.push_back(ZMQ_ERR_NO_ERROR);
+							tx_vect.push_back(dynamixel_ret);
+						} else {
+							tx_error_code=ZMQ_ERR_BUS_OFFLINE;
+						}
+						break;
+					case DYNAMIXEL_RQ_REG_WRITE:
+						if (rx_vect.size()>3) {
+							tx_error_code=ZMQ_ERR_INVALID_PARAMETERS;
+						} else if (dyn_connected==0) {
+							dynamixel_ret=dynamixel_reg_write(
+								dyn,
+								(uint8_t)rx_vect.at(1),
+								(dynamixel_register_t)rx_vect.at(2),
+								rx_vect.size()-3,
+								static_cast<uint8_t*>(rx_vect.data())
+							);
+							tx_vect.push_back(ZMQ_ERR_NO_ERROR);
+							tx_vect.push_back(dynamixel_ret);
+						} else {
+							tx_error_code=ZMQ_ERR_BUS_OFFLINE;
+						}
+						break;
+					case DYNAMIXEL_RQ_REG_ACTION:
+						if (rx_vect.size()!=2) {
+							tx_error_code=ZMQ_ERR_INVALID_PARAMETERS;
+						} else if (dyn_connected==0) {
+							dynamixel_ret=dynamixel_action(dyn,(uint8_t)rx_vect.at(1));
+							tx_vect.push_back(ZMQ_ERR_NO_ERROR);
+							tx_vect.push_back(dynamixel_ret);
+						} else {
+							tx_error_code=ZMQ_ERR_BUS_OFFLINE;
+						}
+						break;
+						
+					case DYNAMIXEL_RQ_RESET:
+						if (rx_vect.size()!=2) {
+							tx_error_code=ZMQ_ERR_INVALID_PARAMETERS;
+						} else if (dyn_connected==0) {
+							dynamixel_ret=dynamixel_reset(dyn,(uint8_t)rx_vect.at(1));
+							tx_vect.push_back(ZMQ_ERR_NO_ERROR);
+							tx_vect.push_back(dynamixel_ret);
+						} else {
+							tx_error_code=ZMQ_ERR_BUS_OFFLINE;
+						}
+						break;
+						
+					case DYNAMIXEL_RQ_SYNC_WRITE:
+						if (rx_vect.size()>3) {
+							tx_error_code=ZMQ_ERR_INVALID_PARAMETERS;
+						} else if (dyn_connected==0) {
+							dynamixel_ret=dynamixel_write_data(
+								dyn,
+								(uint8_t)rx_vect.at(1),
+								(dynamixel_register_t)rx_vect.at(2),
+								rx_vect.size()-3,
+								static_cast<uint8_t*>(rx_vect.data())
+							);
+							tx_vect.push_back(ZMQ_ERR_NO_ERROR);
+							tx_vect.push_back(dynamixel_ret);
+						} else {
+							tx_error_code=ZMQ_ERR_BUS_OFFLINE;
+						}
+						break;
+					case DYNAMIXEL_RQ_ZMQ_ECHO:
+						tx_obj=rx_obj;
+						tx_error_code=0;
+						msgpack::pack(&tx_msg, tx_obj);
+						break;
+						
+					default:
+						tx_error_code=ZMQ_ERR_INVALID_COMMAND;
+				}
+				if (tx_error_code) {
+					tx_vect.push_back(tx_error_code);
+					msgpack::pack(&tx_msg, tx_vect);
+				}
+			} else {
+				/* invalid incomming type, has to be list */
+				std::vector<int> tx_vect;
+				tx_vect.push_back(ZMQ_ERR_INVALID_FORMAT);
+				msgpack::pack(&tx_msg, tx_vect);
+			}
 			
 			zmq::message_t tx_zmq(tx_msg.size());
 			memcpy(static_cast<char*>(tx_zmq.data()), tx_msg.data(), tx_msg.size());
 
+			if (debug) {
+				std::cout << "== tx ==" << std::endl;
+				//std::cout << tx_vect << std::endl;
+			}
 			socket.send(tx_zmq);
 		}
 	}
